9b3e22f6270158280b5f74c9d28669b7d013fdeb
ahpzero <121105267+zeroahp@users.noreply.github.com>
2025-02-09 21:27:57 +0700

    add reponseData

--- backend/src/main/java/com/cocoviet/backend/models/reponse/ResponseData.java
+++ backend/src/main/java/com/cocoviet/backend/models/reponse/ResponseData.java
@@ -1,4 +1,18 @@
 package com.cocoviet.backend.models.reponse;
 
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class ResponseData {
+
+    String status;
+    String msg;
+    Object data;
 }
+
