159c01d1629c0ae9866a3a9146d4e34d66aa6599
ttoannguyen <codehub.personal@gmail.com>
2025-02-09 23:06:10 +0700

    update

--- backend/src/main/java/com/cocoviet/backend/service/ICustomerService.java
+++ backend/src/main/java/com/cocoviet/backend/service/ICustomerService.java
@@ -1,4 +1,5 @@
 package com.cocoviet.backend.service;
 
 public interface ICustomerService {
+
 }
---
+++ backend/src/main/java/com/cocoviet/backend/service/impl/ProductServiceImpl.java
@@ -0,0 +1,14 @@
+package com.cocoviet.backend.service.impl;
+
+import com.cocoviet.backend.models.reponse.ProductResponse;
+import com.cocoviet.backend.models.request.ProductRequest;
+import com.cocoviet.backend.service.IProductService;
+
+public class ProductServiceImpl implements IProductService {
+
+
+    @Override
+    public ProductResponse addProduct(ProductRequest product) {
+
+    }
+}
