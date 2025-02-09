d0f8f734bfce5cc35215cab1653d28d2e90938ad
ahpzero <121105267+zeroahp@users.noreply.github.com>
2025-02-09 23:03:28 +0700

    add depenend pom.xml, update CustomerService and Repository

--- backend/src/main/java/com/cocoviet/backend/service/ICustomerService.java
+++ backend/src/main/java/com/cocoviet/backend/service/ICustomerService.java
@@ -1,4 +1,8 @@
 package com.cocoviet.backend.service;
 
+import com.cocoviet.backend.models.reponse.CustomerResponse;
+import com.cocoviet.backend.models.request.CustomerRequest;
+
 public interface ICustomerService {
+//    CustomerResponse registerCustomer(CustomerRequest customerRequest);
 }
--- backend/src/main/java/com/cocoviet/backend/service/impl/CustomerServiceImpl.java
+++ backend/src/main/java/com/cocoviet/backend/service/impl/CustomerServiceImpl.java
@@ -1,8 +1,27 @@
 package com.cocoviet.backend.service.impl;
 
+import com.cocoviet.backend.models.reponse.CustomerResponse;
+import com.cocoviet.backend.models.request.CustomerRequest;
+import com.cocoviet.backend.repository.ICustomerRepository;
 import com.cocoviet.backend.service.ICustomerService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class CustomerServiceImpl implements ICustomerService {
+
+//    ICustomerRepository iCustomerRepository;
+//
+//    @Override
+//    public CustomerResponse registerCustomer(CustomerRequest customerRequest) {
+//        if(iCustomerRepository.existsByCustomerEmail(customerRequest.getCustomerEmail())) {
+//            throw new RuntimeException("Customer already exists");
+//        }
+//
+//
+//    }
 }
