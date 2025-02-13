dc05e1855e1229a1fcd059051032fc54fd672ca2
ahpzero <121105267+zeroahp@users.noreply.github.com>
2025-02-13 11:53:44 +0700

    change name file

--- backend/src/main/java/com/cocoviet/backend/controller/ProductController.java
+++ backend/src/main/java/com/cocoviet/backend/controller/ProductController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/products")
-public class CoconutProductController {
+public class ProductController {
 
     @Autowired
     IProductService coconutProductService;
