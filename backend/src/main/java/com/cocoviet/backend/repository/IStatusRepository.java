b133e74bc183cbe1d83a4cd43364fa15075ee05a
ttoannguyen <codehub.personal@gmail.com>
2025-02-13 11:51:48 +0700

    create: statusDTO

---
+++ backend/src/main/java/com/cocoviet/backend/repository/IStatusRepository.java
@@ -0,0 +1,10 @@
+package com.cocoviet.backend.repository;
+
+import com.cocoviet.backend.models.entity.StatusEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IStatusRepository extends JpaRepository<StatusEntity, String> {
+    boolean existsByStatusName(String statusName);
+}
