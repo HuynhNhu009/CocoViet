227fcf614d77fd976b9059d28dcc5650d7b092f5
ahpzero <121105267+zeroahp@users.noreply.github.com>
2025-02-19 18:42:12 +0700

    remove product file

--- customer/src/App.jsx
+++ customer/src/App.jsx
@@ -3,7 +3,6 @@ import Home from "./pages/Home";
 import { Route, Routes, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Login from "./pages/Login";
-import ProductListPage from "./pages/ProductListPage";
 import Regis from "./pages/Regis";
 // import ProductList from "./components/ProductList";
 
@@ -22,7 +21,6 @@ function App() {
           location.pathname !== "/register" && <Navbar />}
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/products" element={<ProductListPage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Regis />} />
         </Routes>
