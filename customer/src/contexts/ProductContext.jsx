8f2c4cceda462a2a5435beb92a4b39eeb1564993
ttoannguyen <codehub.personal@gmail.com>
2025-02-19 20:15:53 +0700

    remove context

--- customer/src/contexts/ProductContext.jsx
+++ customer/src/contexts/ProductContext.jsx
@@ -1,30 +1,30 @@
-import React, { createContext, useEffect, useState } from "react";
-import { productService } from "../services/productService";
+// import React, { createContext, useEffect, useState } from "react";
+// import { productService } from "../services/productService";
 
-export const ProductContext = createContext();
+// export const ProductContext = createContext();
 
-export const ProductProvider = ({ children }) => {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+// export const ProductProvider = ({ children }) => {
+//   const [products, setProducts] = useState([]);
+//   const [loading, setLoading] = useState(true);
+//   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      setLoading(true);
-      const { products, error } = await productService.getProducts();
-      console.log("Products in context:", products);
+//   useEffect(() => {
+//     const fetchProducts = async () => {
+//       setLoading(true);
+//       const { products, error } = await productService.getProducts();
+//       console.log("Products in context:", products);
 
-      setProducts(products);
-      setError(error);
-      setLoading(false);
-    };
+//       setProducts(products);
+//       setError(error);
+//       setLoading(false);
+//     };
 
-    fetchProducts();
-  }, []);
+//     fetchProducts();
+//   }, []);
 
-  return (
-    <ProductContext.Provider value={{ products, loading, error }}>
-      {children}
-    </ProductContext.Provider>
-  );
-};
+//   return (
+//     <ProductContext.Provider value={{ products, loading, error }}>
+//       {children}
+//     </ProductContext.Provider>
+//   );
+// };
