4c6c4678b3d2adac1ba31c5aa548d4f0761c0ad8
ahpzero <121105267+zeroahp@users.noreply.github.com>
2025-02-19 21:24:53 +0700

    update product

--- customer/src/components/LoginForm.jsx
+++ customer/src/components/LoginForm.jsx
@@ -31,8 +31,6 @@ function LoginForm() {
 
     // test redux
      dispatch(setLogin(formData));
-
-     navigate("/products")
   };
 
   //test
--- customer/src/components/Products.jsx
+++ customer/src/components/Products.jsx
@@ -1,15 +1,12 @@
 import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 
-import { useSelector } from "react-redux";
 const Products = () => {
 
-  // const login = useSelector((state) => state.CustomerStore.login)
-  // console.log(login);
   
   return (
-    <div className="flex items-center justify-between py-5 font-medium">
-        hello
+    <div className=" bg-amber-200 h-[92vh]">
+        k
     </div>
   );
 };
