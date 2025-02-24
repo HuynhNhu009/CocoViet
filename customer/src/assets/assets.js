480c21c2e61a5d132c58d35ab8883780b91fdfdc
Nguyen Huynh Nhu <chophen26062003@gmail.com>
2025-02-24 07:16:46 +0700

    Update assets.js

--- customer/src/assets/assets.js
+++ customer/src/assets/assets.js
@@ -3,10 +3,16 @@ import bgHome2 from "./TheCoconutTree2.jpg";
 import logo from "./COCOVIET_notext.png";
 import logo2 from "./COCOVIET_whitetext.png";
 import coconutField from "./CoconutField.jpg";
+import retailer1 from "./retailer1.png";
+import retailer2 from "./retailer2.jpg";
+import aboutBackground from "./coconut_leave.jpg"
 export const assets = {
   bgHome1,
   bgHome2,
   logo,
   logo2,
   coconutField,
+  retailer1,
+  retailer2,
+  aboutBackground
 };
