8939c15a952333d99a77623daa99cb85005b94d2
Nguyen Huynh Nhu <chophen26062003@gmail.com>
2025-03-08 06:09:18 +0700

    Add files via upload

---
+++ customer/src/pages/Posts.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import LatestPost from '../components/Post/LatestPost';
+import PostList from '../components/Post/PostList';
+
+const Posts = () => {
+    return (
+        <div>
+            <LatestPost></LatestPost>
+            <PostList></PostList>
+        </div>
+    );
+};
+
+export default Posts;
