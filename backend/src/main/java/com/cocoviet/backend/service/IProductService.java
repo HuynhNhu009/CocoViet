0b66afedb524dd8d718e89154c9d6172f82887c6
ahpzero <121105267+zeroahp@users.noreply.github.com>
2025-03-10 12:04:21 +0700

    update IproductService

--- backend/src/main/java/com/cocoviet/backend/service/IProductService.java
+++ backend/src/main/java/com/cocoviet/backend/service/IProductService.java
@@ -1,5 +1,6 @@
 package com.cocoviet.backend.service;
 
+import com.cocoviet.backend.models.dto.OrderDTO;
 import com.cocoviet.backend.models.dto.ProductDTO;
 import com.cocoviet.backend.models.request.ProductRequest;
 import org.springframework.web.multipart.MultipartFile;
@@ -11,6 +12,7 @@ public interface IProductService {
     ProductDTO addProduct(ProductRequest productRequest, MultipartFile imageFile) throws IOException;
     ProductDTO updateProduct(String productId,ProductRequest productRequest, MultipartFile imageFile) throws IOException;
     ProductDTO getProduct(String productId);
+    ProductDTO deleteVariants(String productId, String variantId);
     List<ProductDTO> getAllProduct();
     List<ProductDTO> getProductByCategory(String categoryId);
     List<ProductDTO> getProductListByRetailerId(String retailerId);
