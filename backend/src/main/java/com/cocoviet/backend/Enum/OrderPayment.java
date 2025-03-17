ecf8cecdc715b7e6ea74d727e77607e37b65325b
ttoannguyen <codehub.personal@gmail.com>
2025-03-17 14:32:52 +0700

    update enum

--- backend/src/main/java/com/cocoviet/backend/Enum/OrderPayment.java
+++ backend/src/main/java/com/cocoviet/backend/Enum/OrderPayment.java
@@ -1,5 +1,8 @@
 package com.cocoviet.backend.Enum;
 
+import lombok.Getter;
+
+@Getter
 public enum OrderPayment {
 
     CASH("CASH", "Thanh toán khi nhận hàng"),
@@ -13,15 +16,7 @@ public enum OrderPayment {
         this.paymentMethod = paymentMethod;
     }
 
-    public String getPaymentCode() {
-        return paymentCode;
-    }
-
-    public String getpaymentMethod() {
-        return paymentMethod;
-    }
-
-    public static OrderPayment frompaymentCode(String paymentCode) {
+    public static OrderPayment fromPaymentCode(String paymentCode) {
         for (OrderPayment status : values()) {
             if (status.paymentCode.equalsIgnoreCase(paymentCode)) {
                 return status;
