4fbb460bde927989810a47aa405f0afe3e902bf6
ttoannguyen <codehub.personal@gmail.com>
2025-03-20 23:40:00 +0700

    done: search retailer product item

--- retailer/src/components/Search/SearchBar.jsx
+++ retailer/src/components/Search/SearchBar.jsx
@@ -36,13 +36,7 @@ const SearchBar = ({
     filterData();
   }, [searchTerm, selectedStatus, selectedCategory, data]);
 
-  const handleSearch = (e) => {
-    setSearchTerm(e.target.value);
-  };
-
-  const handleSearchClick = () => {
-    filterData();
-  };
+  const handleSearch = (e) => setSearchTerm(e.target.value);
 
   const statusOptions = [
     { value: "", label: "Tất cả trạng thái" },
@@ -60,19 +54,18 @@ const SearchBar = ({
     })),
   ];
 
-
   const customStyles = {
     control: (provided) => ({
       ...provided,
       borderRadius: "none",
-      border: "1px solid #16a34a", 
+      border: "1px solid #16a34a",
       "&:hover": {
-        borderColor: "#4b5563", 
-        color: "#16a34a", 
+        borderColor: "#4b5563",
+        color: "#16a34a",
       },
-      fontWeight: "600", 
-      padding: "0 0.5rem", 
-      lineHeight: "1.5rem", 
+      fontWeight: "600",
+      padding: "0 0.5rem",
+      lineHeight: "1.5rem",
       backgroundColor: "white",
       outline: "none",
       boxShadow: "none",
@@ -80,39 +73,53 @@ const SearchBar = ({
     option: (provided, state) => ({
       ...provided,
       borderRadius: "none",
-      backgroundColor: state.isFocused ? "#00A63E" : "white", // hover:bg-green-600
-      color: state.isFocused ? "white" : "black", 
+      backgroundColor: state.isFocused ? "#00A63E" : "white",
+      color: state.isFocused ? "white" : "black",
+    }),
+    dropdownIndicator: (provided) => ({
+      ...provided,
+      color: "#16a34a",
+      padding: "0",
+    }),
+    indicatorSeparator: () => ({
+      display: "none",
     }),
   };
 
   return (
-    <div className="flex w-full items-center gap-2 bg-gray-200 shadow p-2">
-      
-      {isDisplayfilterStatus && filerStatus.length > 0 && (
-        <Select
-          value={statusOptions.find((option) => option.value === selectedStatus)}
-          onChange={(option) => setSelectedStatus(option.value)}
-          options={statusOptions}
-          styles={customStyles}
-          className="w-40"
-        />
-      )}
+    <div className="flex  gap-2 bg-gray-200 shadow p-2 flex-col-reverse sm:flex-row sm:items-center sm:justify-between sm:gap-2">
+      {/* Dropdown filters */}
+      <div className="flex flex-row gap-2 w-full sm:w-auto">
+        {isDisplayfilterStatus && filerStatus.length > 0 && (
+          <Select
+            value={statusOptions.find((option) => option.value === selectedStatus)}
+            onChange={(option) => setSelectedStatus(option.value)}
+            options={statusOptions}
+            styles={customStyles}
+            className="w-2/5 sm:w-40"
+            classNamePrefix="react-select"
+            isSearchable={false}
+          />
+        )}
 
-      {isFilterCategory && filerCategory.length > 0 && (
-        <Select
-          value={categoryOptions.find((option) => option.value === selectedCategory)}
-          onChange={(option) => setSelectedCategory(option.value)}
-          options={categoryOptions}
-          styles={customStyles}
-          className=""
-        />
-      )}
-      
-      <div className="flex items-center ml-auto">
-        <label htmlFor="search" className="font-medium mr-2">
-          SEARCH:
+        {isFilterCategory && filerCategory.length > 0 && (
+          <Select
+            value={categoryOptions.find((option) => option.value === selectedCategory)}
+            onChange={(option) => setSelectedCategory(option.value)}
+            options={categoryOptions}
+            styles={customStyles}
+            className="w-3/5 sm:w-60"
+            classNamePrefix="react-select"
+            isSearchable={false}
+          />
+        )}
+      </div>
+      {/* Search bar */}
+      <div className="flex items-center w-full sm:w-auto sm:ml-auto">
+        <label htmlFor="search" className=" font-medium mr-2 uppercase">
+          Tìm kiếm:
         </label>
-        <div className="relative w-full sm:w-50 group">
+        <div className="relative  sm:w-50 group">
           <input
             id="search"
             type="text"
@@ -125,7 +132,7 @@ const SearchBar = ({
         </div>
         <button
           className="text-green-500 p-2 px-8 cursor-pointer"
-          onClick={handleSearchClick}
+          onClick={filterData}
         >
           <MagnifyingGlassIcon className="h-6 w-6" />
         </button>
