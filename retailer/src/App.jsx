f701e3570a2dd30489f6501933ede3e4f1bbf5f4
ahpzero <121105267+zeroahp@users.noreply.github.com>
2025-03-20 12:56:03 +0700

    update retailer do not coincide with the name

--- retailer/src/App.jsx
+++ retailer/src/App.jsx
@@ -1,8 +1,26 @@
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { retailerAPI } from "../../customer/src/services/retailerService";
 import AllRoute from "./components/AllRoute";
-import React from "react";
+import { setAllRetailer } from "./redux/retailerSlice";
 
 function App() {
-  return < AllRoute  />;
+  const dispatch = useDispatch();
+
+  const getAllRetailer = async () => {
+    try {
+      const responseData = await retailerAPI.getAllRetailer();
+      dispatch(setAllRetailer(responseData.data));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getAllRetailer();
+  }, []);
+
+  return <AllRoute />;
 }
 
 export default App;
