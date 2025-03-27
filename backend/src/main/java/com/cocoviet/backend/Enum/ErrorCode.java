b09b6c2ef05438b5551a38883298ea19f4c86536
ahpzero <121105267+zeroahp@users.noreply.github.com>
2025-03-27 12:49:16 +0700

    update errorCode

--- backend/src/main/java/com/cocoviet/backend/Enum/ErrorCode.java
+++ backend/src/main/java/com/cocoviet/backend/Enum/ErrorCode.java
@@ -10,13 +10,6 @@ import org.springframework.http.HttpStatusCode;
 @AllArgsConstructor
 @NoArgsConstructor
 public enum ErrorCode {
-    USER_EXISTED(1001, "User existed.", HttpStatus.INTERNAL_SERVER_ERROR),
-    USERNAME_INVALID(1002, "Username must be at least 6 characters.", HttpStatus.BAD_REQUEST),
-    PASSWORD_INVALID(1003, "Password must be at least 8 characters.", HttpStatus.BAD_REQUEST),
-    PHONE_NUMBER_INVALID(1004, "Phone number must be at least 10 characters.", HttpStatus.BAD_REQUEST),
-    TITLE_INVALID(1005, "Title must be at least 10 characters.", HttpStatus.BAD_REQUEST),
-    CONTENT_INVALID(1006, "Content must be at least 10 characters.", HttpStatus.BAD_REQUEST),
-    USER_NOT_EXISTED(1007, "User not existed.", HttpStatus.NOT_FOUND),
     UNAUTHENTICATED(1008, "Unauthenticated.", HttpStatus.UNAUTHORIZED),
     UNAUTHORIZED(1009, "You do not have permission.", HttpStatus.FORBIDDEN),
 //    ROLE_EXISTED(1001, "Role existed.", HttpStatus.INTERNAL_SERVER_ERROR);
